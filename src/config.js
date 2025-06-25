1537e76d55d8e2758c54d2faf7f54c2ae21345f3
Chetan8623-hub <you@example.com>
2025-06-25 12:35:48 +0530

    update in cofig.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const WEBSITE_NAME = "Daily Brew ☕️";
+const WEBSITE_NAME = "Daily Brew ☕";
 const WEBSITE_DESC = `Welcome to Daily Brew — your cozy corner for freshly brewed coffee and delicious snacks! 
 From rich espressos to creamy lattes, we craft every cup with love. 
 Sit back, relax, and enjoy the perfect brew, whether you're here to work, chat, or unwind.`;
@@ -6,7 +6,8 @@ Sit back, relax, and enjoy the perfect brew, whether you're here to work, chat, 
 const WEBSITE_URLS = [
   "https://images.unsplash.com/photo-1498804103079-a6351b050096?ixlib=rb-4.0.3&auto=format&fit=crop&w=1470&q=80",
   "https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-4.0.3&auto=format&fit=crop&w=1470&q=80",
- ];
+  "https://images.unsplash.com/photo-1527515637462-cff94eecc1cc?ixlib=rb-4.0.3&auto=format&fit=crop&w=1470&q=80"
+];
 
 const BUTTON1 = "View Menu";
 const BUTTON2 = "Order Now";
